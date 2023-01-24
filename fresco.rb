a28c8e75066cb9a02b333efbe3205c6baa82279c
vanyauhalin <vanyauhalin@gmail.com>
2023-01-24 14:13:29 +0400

    Fix formula sha for 0.1.1

--- fresco.rb
+++ fresco.rb
@@ -2,7 +2,7 @@ class Fresco < Formula
   desc "The handy icon manager for macOS"
   homepage "https://github.com/vanyauhalin/Fresco"
   url "https://github.com/vanyauhalin/Fresco/releases/download/v0.1.1/fresco.tar.zst"
-  sha256 "0a1f56c361229818d41c243a724c4dbeee59d3934793fa9044035be55fb2c4ee"
+  sha256 "1ade3a6f0d4ca282b0defca8c73311424fe4037a446c5be5f94b7fa208c59f71"
   license "MIT"
   head "https://github.com/vanyauhalin/Fresco", branch: "main"
 
